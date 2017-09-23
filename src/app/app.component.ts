325cb2f7176626df32d0d903d734835cbd66bc17
eguo <eguo@localhost.localdomain>
2017-09-23 15:24:49 -0400

    added title

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,31 @@
 import { Component } from '@angular/core';
+import {NavigationEnd, NavigationStart, Router} from "@angular/router";
+import {IHash} from "./models/ihash";
 
 @Component({
     selector: 'app-root',
     templateUrl: './app.component.html',
     styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-    title = 'Tour of Heroes';
+
+
+export class AppComponent{
+    titleDefault = 'Highway Management System' ;
+    title;
+
+    constructor(private router: Router){
+      let pathToTitle = {};
+      pathToTitle["/add-new-highway"] = "Add New Highway";
+      pathToTitle["/add-segment"] = "Add New Segment";
+
+      router.events.subscribe(
+        (event)=>{
+          if(event instanceof NavigationEnd)
+          {
+            this.title = pathToTitle[event.url];
+            if(!this.title) this.title=this.titleDefault;
+          }
+        }
+      );
+    }
 }
