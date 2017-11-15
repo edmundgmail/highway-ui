c23200d124759ebbf9edcee5c119a111f458d6ae
Edmund Guo <edmund.guo@rbc.com>
2017-11-15 09:20:17 -0500

    update-with add seg works

--- src/app/add-segment/add-segment.component.ts
+++ src/app/add-segment/add-segment.component.ts
@@ -1,12 +1,13 @@
 import { Component, OnInit } from '@angular/core';
 import {FormBuilder, FormControl, FormGroup, Validators} from "@angular/forms";
-import {Highway} from '../models/highway';
+import {AddSegmentRecord, Highway} from '../models/highway';
 import {HighwayService} from "../services/highway.service";
-import {Observable} from 'rxjs/Observable';
 import 'rxjs/add/operator/startWith';
 import 'rxjs/add/operator/map';
 import {Project} from "../models/project";
-
+import {SegmentPoint} from "../models/segment-point";
+import {Http, RequestOptions, Response} from '@angular/http';
+import {Headers} from '@angular/http';
 
 @Component({
   selector: 'app-add-segment',
@@ -15,14 +16,16 @@ import {Project} from "../models/project";
 })
 
 export class AddSegmentComponent implements OnInit {
-  start;
-  end;
+  start : SegmentPoint;
+  end : SegmentPoint;
   newseg;
   project: Project;
   currentHighway;
   currentDir;
+  editDate: FormControl;
 
-  constructor(private formBuilder: FormBuilder, private highwayService: HighwayService) {
+  constructor(private formBuilder: FormBuilder, private highwayService: HighwayService, private http: Http) {
+    this.editDate = new FormControl();
     this.highwayService.currentHighwaySelected$.subscribe(value => this.currentHighway = value);
     this.highwayService.currentDirSelected$.subscribe(value => this.currentDir = value);
   }
@@ -56,6 +59,31 @@ export class AddSegmentComponent implements OnInit {
     console.log('endRP=' + JSON.stringify(this.end));
     console.log('newseg=' + this.newseg);
     console.log("project=" + this.project);
+
+    let record = new AddSegmentRecord();
+    record.roadId = this.currentHighway.roadId;
+    record.action = 'AddSegmentRecord';
+    record.dateTime = this.editDate.value;
+    record.dir = this.currentDir;
+    record.afterRP = this.start.rp.name;
+    record.beforeRP = this.end.rp.name;
+    record.leftConnect = this.start.connect;
+    record.rightConnect = this.end.connect;
+    record.segment = this.newseg;
+
+    this.postNewSegment(record);
+
+  }
+
+  postNewSegment(o: Object) {
+    let body = JSON.stringify(o);
+    console.log(body)
+    //this.http.get('http://localhost:5000/highway').forEach( res=> console.log(res));
+
+    let headers = new Headers({ 'Content-Type': 'application/json' });
+    let options = new RequestOptions();
+    options.headers = headers;
+    return this.http.post('http://localhost:5000/highway', body, options).forEach(res=>console.log(res.toString()));
   }
 
   ngOnInit() {
